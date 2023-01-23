1306d4da4d24a60eb17a9dc5011b50fbba89d05e
Clayton Lawton <cla10law10@gmail.com>
2023-01-23 13:11:00 -0800

    logic_ex coded
    Coded the project using Vivado on Linux. Pushing to git in order to pull on Windows and simulate. Will correct simulating errors on Linux soon.

--- src/logic_ex.vhd
+++ src/logic_ex.vhd
@@ -8,8 +8,16 @@ end logic_ex;
 
 architecture Behavioral of logic_ex is
 
+    signal not1, and1, or1, xor1 : std_logic := '0';
+
 begin
 
+    not1 <= NOT SW(0);
+    and1 <= SW(1) AND SW(0);
+    or1 <= SW(1) OR SW(0);
+    xor1 <= SW(1) XOR SW(0);
+    
+    LED <= xor1 & or1 & and1 & not1;
 
 end Behavioral;
 
