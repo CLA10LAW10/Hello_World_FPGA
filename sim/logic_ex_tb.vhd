1a6c41008d16f0998efc1add1aa98976d5950084
Clayton Lawton <cla10law10@gmail.com>
2023-01-26 09:55:46 -0800

    VHDL Simulation Update
    Correcting syntax in the file

--- sim/logic_ex_tb.vhd
+++ sim/logic_ex_tb.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 use std.env.stop;
+use ieee.numeric_std.all ;
 
 entity logic_ex_tb is
 end logic_ex_tb;
@@ -12,8 +14,8 @@ component logic_ex is
            LED : out STD_LOGIC_VECTOR (3 downto 0));
 end component logic_ex;
 
-    signal SW_tb : STD_LOGIC_VECTOR (1 downto 0)
-    signal LED_tb : TD_LOGIC_VECTOR (3 downto 0);
+    signal SW_tb : STD_LOGIC_VECTOR (1 downto 0);
+    signal LED_tb : STD_LOGIC_VECTOR (3 downto 0);
 	
 begin
 
@@ -22,34 +24,38 @@ begin
 	--Stimulus
 	process 
 	begin
-		$printtimescale(logic_ex_tb);
-		SW_tb <= 00;
+		--$printtimescale(logic_ex_tb);
+		SW_tb <= "00";
 		for i in 0 to 10 loop
-			report "Setting switches to" & integer'image(SW_tb(1:0));
-			SW_tb <= i(1:0);
+			report "Setting switches to" & integer'image(SW_tb);
+			SW_tb <= i;
 			wait for 100 ns;
 		end loop;
 		report "PASS: logic_ex test PASSED!";
-    end process
+    end process;
 	
 	-- Checking
-	process (LED) begin
+	process (LED_tb) begin
 	
-		if (NOT SW_tb(0) !== LED(0)) then
+		if (NOT SW_tb(0) /= LED(0)) then
 			report"FAIL: NOT Gate mismatch";
 			stop;
+		end if;
 			
-		if ((SW_tb(0) AND SW_tb(1)) !== LED(1)) then
+		if ((SW_tb(0) AND SW_tb(1)) /= LED(1)) then
 			report"FAIL: AMD Gate mismatch";
 			stop;
+		end if;
 			
-		if ((SW_tb(0) OR SW_tb(1)) !== LED(2)) then
+		if ((SW_tb(0) OR SW_tb(1)) /= LED(2)) then
 			report"FAIL: OR Gate mismatch";
 			stop;
+		end if;
 			
-		if ((SW_tb(0) XOR SW_tb(1)) !== LED(3)) then
+		if ((SW_tb(0) XOR SW_tb(1)) /= LED(3)) then
 			report"FAIL: XOR Gate mismatch";
 			stop;
+		end if;
 		
-	end
+	end process;
 end Behavioral ;
